316ea8c1d9e3474fe66f67993be4df065f7e37fb
bonbo0811 <abcdef999gp811@gmail.com>
2022-05-17 00:07:37 +0900

    topページ追加、old関数

---
+++ resources/views/kakeibo/top.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="mt-3"></div>
+        <h1 class="mt-5 text-white fw-bolder">カケイボ！を始めよう。 </h1>
+            <div class="col-md-8 mt-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="mb-0 fw-bold">
+                        「カケイボ！」とは？
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="col-md-9 mt-2 mb-3 m-auto">
+                            <img src="{{ asset('images/toppic.png')}}" class="img-fluid border" alt="カケイボ！のイメージ画像">
+                        </div>
+                        <div class="px-4 py-2 ms-4 me-4">
+                            <p class="fw-bold">「カケイボ！」は出費をジャンルごとに1カ月単位で管理できるクラウド家計簿のアプリです。</p>
+                            <p class="fw-bold">カケイボ！を使えば今月使ったお金を食費、日用品、交際費、光熱費などのジャンルに分け、今月いくら使ったかをすぐにいつでも登録し、簡単に確認出来ます。</p>
+                            <p class="fw-bold">スマートフォンからだけでなくパソコン、タブレットからも楽々操作出が可能です。</p>
+                        </div>
+                        <div class="row mb-0 text-center">
+                            <div>
+                                <a href="{{ route('login') }}" class="me-1 btn btn-primary">ログイン</a>
+                                <a href="{{ route('register') }}" class="ms-1 btn btn-primary">新規登録</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+@endsection
